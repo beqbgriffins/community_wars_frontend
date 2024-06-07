8a2024282195455898f67106bbe05326eceb6c29
Gleb Omarov <wallt.dv@gmail.com>
2024-06-08 02:30:18 +0400

    add hero equipment section

--- src/components/content/content.js
+++ src/components/content/content.js
@@ -2,6 +2,19 @@ import "../../App.css";
 export default function Content() {
     return (
         <div className="content">
+            <div className="image">
+                <div className="square_item head"></div>
+                <div className="circle_item amulet"></div>
+                <div className="rectangle_item body"></div>
+                <div className="rectangle_item left-hand">
+                    <div className="dagger"></div>
+                </div>
+                <div className="rectangle_item right-hand"></div>
+                <div className="circle_item left-ring"></div>
+                <div className="square_item gloves"></div>
+                <div className="square_item feet"></div>
+                <div className="circle_item right-ring"></div>
+            </div>
         </div>
     )
 }
